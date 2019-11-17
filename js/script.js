a2bce003f3a6ea3f141c39c71e2b08b6cdba0284
Den Ulyanchenko <denulyanchenko@gmail.com>
2019-11-17 14:52:16 +0200

    notification

--- js/script.js
+++ js/script.js
@@ -11,19 +11,44 @@ console.log(userName);
 console.log(person);
 var called = false;
 
+function spawnNotification(body, icon, title) {
+  var options = {
+      body: body,
+      icon: icon
+  };
+  var n = new Notification(title, options);
+}
+
 button.onclick = function(){
-    called = true;
+    if (called) {
+        called = false;
+    } else called = true;
     animate(called);
-    prompt("Set your billing account");
-    var notification = new Notification('Succes!');
-    //setTimeout(notification.close.bind(notification), 4000);
-};
+    //prompt("Set your billing account");
+}
+
+function notifyMe() {
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  else if (Notification.permission === "granted") {
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission === "denied") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+}
 
 function animate(called){
     button.style.transitionDuration = '800ms';
     if (called) {
         button.style.transform = 'rotate(270deg)';
-    } else {
-        button.style.transform = 'rotate(0deg)'
-    }
+    } else button.style.transform = 'rotate(0deg)';
 };
