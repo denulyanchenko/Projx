8e3d21806de42c0c9c3e19717d27cc6831dd30e1
Den Ulyanchenko <denulyanchenko@gmail.com>
2019-11-20 02:03:42 +0200

    change 20/11/2019

--- js/script.js
+++ js/script.js
@@ -1,54 +1,18 @@
-var userName;
-var button = document.getElementById('button');
+var isTooSoon = true;
+window.addEventListener("beforeinstallprompt", function(e) {
+  if (isTooSoon) {
+    e.preventDefault(); // Prevents prompt display
+    // Prompt later instead:
+    setTimeout(function() {
+      isTooSoon = false;
+      e.prompt(); // Throws if called more than once or default not prevented
+    }, 1000);
+  }
 
-var person = {
-    firstName: 'Den',
-    lastName: 'Ulyanchenko',
-    id: 1305
-};
-userName = person.firstName;
-console.log(userName);
-console.log(person);
-var called = false;
-
-function spawnNotification(body, icon, title) {
-  var options = {
-      body: body,
-      icon: icon
-  };
-  var n = new Notification(title, options);
-}
+  // The event was re-dispatched in response to our request
+  // ...
+});
 
 button.onclick = function(){
-    if (called) {
-        called = false;
-    } else called = true;
-    animate(called);
-    //prompt("Set your billing account");
-}
-
-function notifyMe() {
-  if (!("Notification" in window)) {
-    alert("This browser does not support desktop notification");
-  }
-
-  else if (Notification.permission === "granted") {
-    var notification = new Notification("Hi there!");
-  }
-
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission === "denied") {
-    Notification.requestPermission().then(function (permission) {
-      if (permission === "granted") {
-        var notification = new Notification("Hi there!");
-      }
-    });
-  }
-}
-
-function animate(called){
-    button.style.transitionDuration = '800ms';
-    if (called) {
-        button.style.transform = 'rotate(270deg)';
-    } else button.style.transform = 'rotate(0deg)';
+    prompt("Set your billing account");
 };
