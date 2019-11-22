49a340b6b1abf88bd364adcadc230fd2609ddf81
Den Ulyanchenko <denulyanchenko@gmail.com>
2019-11-23 01:17:53 +0200

    add script

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
-var isTooSoon = true;
-window.addEventListener("beforeinstallprompt", function(e) {
-  if (isTooSoon) {
-    e.preventDefault(); // Prevents prompt display
-    // Prompt later instead:
-    setTimeout(function() {
-      isTooSoon = false;
-      e.prompt(); // Throws if called more than once or default not prevented
-    }, 1000);
-  }
+let button = document.getElementById('button');
 
-  // The event was re-dispatched in response to our request
-  // ...
-});
-
-button.onclick = function(){
-    prompt("Set your billing account");
+button.onclick = ()=> {
+    let r = confirm('Do you want take image from server?');
+    if (r == true){
+        alert('Ok, see this?');
+    } else alert('Sorry, maybe later?');
 };
