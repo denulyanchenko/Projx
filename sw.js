b63865c7465bdcff6773e520a460dba11ac40edf
Den Ulyanchenko <denulyanchenko@gmail.com>
2019-12-30 00:31:03 +0200

    add pwa files

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/img/main.jpg',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate') {
+      event.respondWith(fetch('/pwa'));
+
+      fetch(event.request.url);
+    }
+
+});
